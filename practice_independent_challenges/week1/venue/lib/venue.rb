44089040a32cce757afe585647407258c3e21184
Sunny Moore <moore.sunny@gmail.com>
2023-08-22 13:41:46 -0600

    Iteration 1 and 2

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,17 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(name)
+    @patrons.push(name)
+  end
+
+  def yell_at_patrons
+    @patrons.map {|item| item.upcase }
+  end
 end
