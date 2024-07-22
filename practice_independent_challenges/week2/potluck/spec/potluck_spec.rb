f85012873ea3b10c28f27ac6ba6bff411f8629d9
Sunny Moore <moore.sunny@gmail.com>
2024-07-22 15:58:14 -0700

    Test add_dish method, create method

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -22,5 +22,18 @@ describe 'Potluck' do
       expect(potluck.dishes.length).to eq 0
     end
   end
-  
+  describe 'add_dish method' do
+    it 'can add dishes' do
+      dish1 = Dish.new("Couscous Salad", :appetizer)
+      dish2 = Dish.new("Cocktail Meatballs", :entre)
+
+      potluck = Potluck.new("7-21-24")
+      potluck.add_dish(dish1)
+      potluck.add_dish(dish2)
+
+      expect(potluck.dishes.length).to eq 2
+      expect(potluck.dishes[0]).to eq dish1
+      expect(potluck.dishes[1]).to eq dish2
+    end
+  end
 end
